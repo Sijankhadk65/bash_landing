fdd740de1b700984866a53ea9cea9dd411a6cf66
USER <user@USERs-MacBook-Pro.local>
2023-01-29 00:15:31 +0545

    Title Change

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Bash Nepal | Opening Soon!</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
